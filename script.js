1ab49870affb8a5dc574dab39e4a92136ab0c97f
Jawad-Alichd4 <159112594+Jawad-Alichd4@users.noreply.github.com>
2024-09-15 10:31:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+var btn1 = document.getElementById("btn");
+var bd = document.body;
+btn1.className= "daymode";
+bd.className = "daymode";
+
+
+function changemode(){
+if (btn1.className === "darkmode") {
+    btn1.className = "daymode"; 
+    btn1.style.backgroundColor = "black"
+    btn1.innerHTML = "day";
+    bd.style.backgroundColor="white";
+}
+else if (btn1.className === "daymode" ) {
+    btn1.className = "darkmode";
+    btn1.style.backgroundColor="white"
+
+    btn1.innerHTML = "night";
+    bd.className = "darkmode"; 
+    bd.style.backgroundColor="black";
+}
+}
